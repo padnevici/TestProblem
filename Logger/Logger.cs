f3b5fed835f681c5375b2c38455b3e16da7f3905
padnevici <padnevici@gmail.com>
2015-02-27 22:18:43 -0500

    Created a structure of feature framework

---
+++ Logger/Logger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+namespace Logger
+{
+    public class Logger
+    {
+        private static Logger instance;
+        private static readonly ILog log = LogManager.GetLogger(typeof(Object));
+
+        private Logger()
+        {
+            string config = System.IO.File.ReadAllText("Log4netCFG.txt");
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            int secondsSinceEpoch = (int)t.TotalSeconds;
+            System.IO.File.WriteAllText("Log4netCFG.xml", config.Replace("%date%", secondsSinceEpoch.ToString()));
+            System.IO.Directory.CreateDirectory("Logs");
+
+            XmlConfigurator.Configure(new System.IO.FileInfo("Log4netCFG.xml"));
+        }
+
+        public void Debug(string message)
+        {
+            log.Debug(message);
+        }
+
+        public void Warn(string message)
+        {
+            log.Warn(message);
+        }
+
+        public void Info(string message)
+        {
+            log.Info(message);
+        }
+
+        public void Error(string message)
+        {
+            log.Error(message);
+        }
+
+        public void Fatal(string message)
+        {
+            log.Fatal(message);
+        }
+
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Logger();
+                }
+                return instance;
+            }
+        }
+    }
+}
---
+++ TopTal_TestCases/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopTal_TestCases
+{
+    public class Class1
+    {
+    }
+}
