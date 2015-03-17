fafe69e00bb32cd4517649d94a3f8e4f0cf249d9
padnevici <padnevici@gmail.com>
2015-03-16 23:09:03 -0400

    - restructd the structure of framework, moved some files required for intermediate steps before run

---
+++ localLyb/RegisterAutoIt.bat
@@ -0,0 +1,3 @@
+cd %~dp0
+regsvr32 autoit-v3\install\AutoItX\AutoItX3.dll
+regedit /s DllSurrogate.reg
---
+++ localLyb/RunTests.bat
@@ -0,0 +1,2 @@
+cd %~dp0
+"TopTal_Framework\packages\NUnit.Runners.2.6.4\tools\nunit.exe" .\TopTalTests\TopTal_TestCases.dll /run
